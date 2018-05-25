0b6f55aacadb1721bf676ac751b4976219e71128
jiyangg <408776303@qq.com>
2018-05-25 11:00:50 +0800

    update logger

--- pear/utils/logger.py
+++ pear/utils/logger.py
@@ -1,16 +1,17 @@
 #! coding=utf-8
 import logging
+
 import coloredlogs
 
-from pear.utils.config import LOGGING_FORMATTER, IS_DEBUG
+from pear.utils.config import LOGGING_FORMATTER
 
 logging.basicConfig(format=LOGGING_FORMATTER, level=logging.INFO)
 logger = logging.getLogger('')
-if not IS_DEBUG:
-    logger_handler = logging.FileHandler('log')
-    logger_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter(LOGGING_FORMATTER)
-    logger_handler.setFormatter(formatter)
-    logger.addHandler(logger_handler)
+
+logger_handler = logging.FileHandler('log')
+logger_handler.setLevel(logging.INFO)
+formatter = logging.Formatter(LOGGING_FORMATTER)
+logger_handler.setFormatter(formatter)
+logger.addHandler(logger_handler)
 
 coloredlogs.install(level='INFO', logger=logger, fmt=LOGGING_FORMATTER)
